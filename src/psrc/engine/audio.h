36063a80da25b20b9d48b6d3486f805a9a2edaaf
PQCraft <0456523@gmail.com>
2025-02-01 17:43:21 -0500

    Starting audio rewrite

--- src/psrc/engine/audio.h
+++ src/psrc/engine/audio.h
@@ -33,17 +33,8 @@
             - setMusicStyle
 */
 
-#include "../platform.h"
-
+#include "../attribs.h"
 #include "../resource.h"
-#include "../threading.h"
-
-#include "../../stb/stb_vorbis.h"
-#ifdef PSRC_USEMINIMP3
-    #include "../../minimp3/minimp3_ex.h"
-#endif
-
-#include "../platform.h"
 
 #if PLATFORM == PLAT_NXDK || PLATFORM == PLAT_GDK
     #include <SDL.h>
@@ -56,81 +47,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "../attribs.h"
-
-struct audioemitter {
-    int max;
-    int uses;
-    uint8_t paused : 1;
-    uint8_t bg : 1;
-    float vol[2];
-    float speed;
-    float pos[3];
-    float range;
-    float lpfilt;
-    float hpfilt;
-};
-
-struct audiosound_audbuf {
-    int off;
-    int len;
-    union {
-        int16_t* data[2];
-        #ifdef PSRC_USEMINIMP3
-        mp3d_sample_t* data_mp3;
-        #endif
-    };
-};
-struct audiosound_fx {
-    int posoff; // position offset in output freq samples (based on the dist between campos and pos)
-    int speedmul; // speed mult in units of 32 (based on speed)
-    int volmul[3]; // volume mult in units of 32768 (based on vol, camrot, and the dist between campos and pos)
-    int lpfiltmul; // from 0 to output freq
-    int hpfiltmul; // from 0 to output freq
-};
-struct audiosound {
-    struct rc_sound* rc;
-    union {
-        stb_vorbis* vorbis;
-        #ifdef PSRC_USEMINIMP3
-        mp3dec_ex_t* mp3;
-        #endif
-    };
-    struct audiosound_audbuf audbuf;
-    long offset;
-    int frac;
-};
-struct audiosound_alert {
-    struct audiosound data;
-    int8_t prio;
-};
-struct audiosound_3d {
-    struct audiosound data;
-    int emitter;
-    uint8_t flags : 7;
-    uint8_t fxchanged : 1;
-    int fxoff;
-    float vol[2];
-    float speed;
-    float pos[3];
-    float range;
-    float lpfilt;
-    float hpfilt;
-    struct audiosound_fx fx[2];
-    int maxvol;
-    int16_t lplastout;
-    int16_t hplastout;
-    int16_t hplastin;
-};
-
-struct audiovoicegroup_world {
-    struct audiosound_3d* data;
-    int* sortdata;
-    int len;
-    int size;
-    int playcount;
-};
 struct audiostate {
+    struct {
+        float pos[3];
+        float rot[3];
+        float rotradx, rotrady, rotradz;
+        float sinx, cosx;
+        float siny, cosy;
+        float sinz, cosz;
+    } cam;
     #ifndef PSRC_NOMT
     struct accesslock lock;
     #endif
@@ -141,170 +66,105 @@ struct audiostate {
     #endif
     int freq;
     int channels;
-    volatile int8_t audbufindex;
-    volatile int8_t mixaudbufindex;
-    int audbuflen;
-    unsigned outbufcount;
     struct rcopt_sound soundrcopt;
-    struct {
-        int16_t* out[2];
-        unsigned outsize;
-        int* data[2][2];
-        int len;
-    } audbuf;
-    struct {
-        struct audioemitter* data;
-        int len;
-        int size;
-    } emitters;
-    struct {
-        struct audiosound ui;
-        struct {
-            struct audiosound_alert* data;
-            int count;
-            int next;
-        } alerts;
-        struct audiovoicegroup_world world;
-        struct audiovoicegroup_world worldbg;
-        struct {
-            struct rc_sound* queue;
-            struct audiosound data[2];
-            float fade;
-            float oldfade;
-            uint8_t index;
-        } ambience;
-        struct {
-            struct rc_sound* queue;
-            struct audiosound data;
-            float vol;
-            float oldvol;
-        } music;
-    } voices;
-    struct {
-        uint8_t master;
-        uint8_t ui;
-        uint8_t alerts;
-        uint8_t world;
-        uint8_t worldbg;
-        uint8_t ambience;
-        uint8_t music;
-        uint8_t voice;
-    } vol;
-    struct {
-        uint8_t filterchanged : 1;
-        uint8_t reverbchanged : 1;
-        struct {
-            float amount[2];
-            int lastout[2];
-        } lpfilt;
-        struct {
-            float amount[2];
-            int lastout[2];
-            int lastin[2];
-        } hpfilt;
-        struct {
-            float delay[2];
-            float feedback[2];
-            float mix[2];
-            int16_t* buf[2];
-            unsigned len;
-            unsigned size;
-            unsigned head;
-            struct {
-                float amount[2];
-                int16_t lastout[2];
-            } lpfilt;
-            struct {
-                float amount[2];
-                int16_t lastout[2];
-                int16_t lastin[2];
-            } hpfilt;
-        } reverb;
-    } env;
-    struct {
-        float pos[3];
-        float rot[3];
-        float rotradx, rotrady, rotradz;
-        float sinx, cosx;
-        float siny, cosy;
-        float sinz, cosz;
-    } cam;
 };
 
 extern struct audiostate audiostate;
 
 enum audioopt {
     AUDIOOPT_END,
-    AUDIOOPT_FREQ, // int
-    AUDIOOPT_VOICES // int
+    AUDIOOPT_FREQ,      // int
+    AUDIOOPT_3DVOICES,  // int
+    AUDIOOPT_2DVOICES   // int
 };
+
+#define AUDIOPRIO_DEFAULT (-128)
+#define AUDIOPRIO_MIN (-127)
+#define AUDIOPRIO_NORMAL (0)
+#define AUDIOPRIO_MAX (127)
+
+#define AUDIOFXMASK_VOL_L (1 << 0)
+#define AUDIOFXMASK_VOL_R (1 << 1)
+#define AUDIOFXMASK_VOL (AUDIOFXMASK_VOL_L | AUDIOFXMASK_VOL_R)
+#define AUDIOFXMASK_SPEED (1 << 2)
+#define AUDIOFXMASK_LPFILT (1 << 3)
+#define AUDIOFXMASK_HPFILT (1 << 4)
+#define AUDIOFXMASK_ALL (-1U)
+struct audiofx {
+    uint8_t vol[2];
+    float speed; 
+    float lpfilt;
+    float hpfilt;
+};
+
+#define AUDIO3DFXMASK_POS (1 << 0)
+#define AUDIO3DFXMASK_SIZE (1 << 1)
+#define AUDIO3DFXMASK_VOLDAMP (1 << 2)
+#define AUDIO3DFXMASK_FREQDAMP (1 << 3)
+#define AUDIO3DFXMASK_NODOPPLER (1 << 4)
+#define AUDIO3DFXMASK_RELPOS (1 << 5)
+#define AUDIO3DFXMASK_RELROT (1 << 6)
+#define AUDIO3DFXMASK_ALL (-1U)
+struct audio3dfx {
+    float pos[3];
+    float size[3];
+    float voldamp;
+    float freqdamp;
+    uint8_t nodoppler : 1;
+    uint8_t relpos : 1;
+    uint8_t relrot : 1;
+};
+
+#define AUDIOENVMASK_LPFILT (1 << 0)
+#define AUDIOENVMASK_HPFILT (1 << 1)
+#define AUDIOENVMASK_REVERB_DELAY (1 << 2)
+#define AUDIOENVMASK_REVERB_FEEDBACK (1 << 3)
+#define AUDIOENVMASK_REVERB_MIX (1 << 4)
+#define AUDIOENVMASK_REVERB_LPFILT (1 << 5)
+#define AUDIOENVMASK_REVERB_HPFILT (1 << 6)
+#define AUDIOENVMASK_REVERB (AUDIOENVMASK_REVERB_DELAY | AUDIOENVMASK_REVERB_FEEDBACK | AUDIOENVMASK_REVERB_MIX | \
+                            AUDIOENVMASK_REVERB_LPFILT | AUDIOENVMASK_REVERB_HPFILT)
+#define AUDIOENVMASK_ALL (-1U)
+struct audioenv {
+    float lpfilt;
+    float hpfilt;
+    struct {
+        float delay;
+        float feedback;
+        float mix;
+        float lpfilt;
+        float hpfilt;
+    } reverb;
+};
+
+#define SOUNDFLAG_LOOP (1 << 0)
+#define SOUNDFLAG_WRAP (1 << 1)
 
 bool initAudio(void);
 bool startAudio(void);
 void updateAudioConfig(enum audioopt, ...);
+void updateAudio(float framemult);
 void stopAudio(void);
 bool restartAudio(void);
-void quitAudio(void);
+void quitAudio(bool quick);
 
-#define SOUNDFLAG_LOOP (1 << 0)
-#define SOUNDFLAG_WRAP (1 << 1)
-#define SOUNDFLAG_NODOPPLER (1 << 2)
+int new3DAudioEmitter(int8_t prio, unsigned max, unsigned fxmask, const struct audiofx*, unsigned fx3dmask, const struct audio3dfx*);
+void edit3DAudioEmitter(int, unsigned fxmask, const struct audiofx*, unsigned fx3dmask, const struct audio3dfx*);
+void pause3DAudioEmitter(int, bool);
+void stop3DAudioEmitter(int);
+void delete3DAudioEmitter(int);
+bool play3DSound(int e, struct rc_sound* rc, int8_t prio, uint8_t flags, unsigned fxmask, const struct audiofx*);
 
-enum soundfx {
-    SOUNDFXENUM__END,
-    SOUNDFXENUM_VOL, // double, double
-    SOUNDFXENUM_SPEED, // double
-    SOUNDFXENUM_POS, // double, double, double
-    SOUNDFXENUM_RANGE, // double
-    SOUNDFXENUM_LPFILT, // double
-    SOUNDFXENUM_HPFILT // double
-};
-#define SOUNDFX_VOL(l, r) SOUNDFXENUM_VOL, (double)(l), (double)(r)
-#define SOUNDFX_SPEED(m) SOUNDFXENUM_SPEED, (double)(m)
-#define SOUNDFX_POS(x, y, z) SOUNDFXENUM_POS, (double)(x), (double)(y), (double)(z)
-#define SOUNDFX_RANGE(a) SOUNDFXENUM_RANGE, (double)(a)
-#define SOUNDFX_LPFILT(a) SOUNDFXENUM_LPFILT, (double)(a)
-#define SOUNDFX_HPFILT(a) SOUNDFXENUM_HPFILT, (double)(a)
+int new2DAudioEmitter(int8_t prio, unsigned max, unsigned fxmask, const struct audiofx*);
+void edit2DAudioEmitter(int, unsigned fxmask, const struct audiofx*);
+void pause2DAudioEmitter(int, bool);
+void stop2DAudioEmitter(int);
+void delete2DAudioEmitter(int);
+bool play2DSound(int e, struct rc_sound* rc, int8_t prio, uint8_t flags, unsigned fxmask, const struct audiofx*);
 
-enum soundenv {
-    SOUNDENVENUM__END,
-    SOUNDENVENUM_LPFILT, // double
-    SOUNDENVENUM_HPFILT, // double
-    SOUNDENVENUM_REVERB_DELAY, // double
-    SOUNDENVENUM_REVERB_FEEDBACK, // double
-    SOUNDENVENUM_REVERB_MIX, // double
-    SOUNDENVENUM_REVERB_LPFILT, // double
-    SOUNDENVENUM_REVERB_HPFILT, // double
-};
-#define SOUNDENV_LPFILT(a) SOUNDENVENUM_LPFILT, (double)(a)
-#define SOUNDENV_HPFILT(a) SOUNDENVENUM_HPFILT, (double)(a)
-#define SOUNDENV_REVERB(d, f, m, lp, hp) SOUNDENVENUM_REVERB_DELAY, (double)(d), SOUNDENVENUM_REVERB_FEEDBACK, (double)(f),\
-    SOUNDENVENUM_REVERB_MIX, (double)(m), SOUNDENVENUM_REVERB_LPFILT, (double)(lp), SOUNDENVENUM_REVERB_HPFILT, (double)(hp)
-#define SOUNDENV_REVERB_DELAY(d) SOUNDENVENUM_REVERB_DELAY, (double)(d)
-#define SOUNDENV_REVERB_FEEDBACK(f) SOUNDENVENUM_REVERB_FEEDBACK, (double)(f)
-#define SOUNDENV_REVERB_MIX(m) SOUNDENVENUM_REVERB_MIX, (double)(m)
-#define SOUNDENV_REVERB_LPFILT(lp) SOUNDENVENUM_REVERB_LPFILT, (double)(lp)
-#define SOUNDENV_REVERB_HPFILT(hp) SOUNDENVENUM_REVERB_HPFILT, (double)(hp)
+void setAudioEnv(unsigned mask, struct audioenv*);
 
-int newAudioEmitter(int max, unsigned /*bool*/ bg, ... /*soundfx*/);
-#define newAudioEmitter(...) newAudioEmitter(__VA_ARGS__, SOUNDFXENUM__END)
-void editAudioEmitter(int, unsigned /*bool*/ immediate, ... /*soundfx*/);
-#define editAudioEmitter(...) editAudioEmitter(__VA_ARGS__, SOUNDFXENUM__END)
-void pauseAudioEmitter(int, bool);
-void stopAudioEmitter(int);
-void deleteAudioEmitter(int);
-
-void playSound(int emitter, struct rc_sound* rc, unsigned /*uint8_t*/ flags, ... /*soundfx*/);
-#define playSound(...) playSound(__VA_ARGS__, SOUNDFXENUM__END)
-void playUISound(struct rc_sound* rc);
-void playAlertSound(struct rc_sound* rc); // TODO: maybe add speed?
-void setAmbientSound(struct rc_sound* rc);
 void setMusic(struct rc_sound* rc); // TODO: make rc_music once PTM is added
 void setMusicStyle(const char*);
-
-void editSoundEnv(enum soundenv, ...);
-#define editSoundEnv(...) editSoundEnv(__VA_ARGS__, SOUNDENVENUM__END)
-
-void updateSounds(float framemult);
 
 #endif
--- src/psrc/main/engine.c
+++ src/psrc/main/engine.c
@@ -13,9 +13,6 @@
 
 struct rc_script* mainscript;
 
-static struct rc_sound* test;
-static int testemt_map;
-static int testemt_obj;
 static float lookspeed[2];
 static uint64_t toff;
 static uint64_t framestamp;
@@ -249,43 +246,10 @@ static int bootstrap(void) {
         cb_dump(&e);
     }
 
-    testemt_map = newAudioEmitter(1, true);
-    testemt_obj = newAudioEmitter(1, false, SOUNDFX_POS(0.0, 0.0, 3.0));
+    // TODO: redo audio demo
 
-    if ((test = getRc(RC_SOUND, "sounds/ambient/wind1", &audiostate.soundrcopt, 0, NULL))) {
-        //setAmbientSound(test);
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/ac1", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, SOUNDFLAG_LOOP | SOUNDFLAG_WRAP, SOUNDFX_POS(0.0, 0.0, 2.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/siren", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, 0, SOUNDFX_POS(0.0, 1.0, 0.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/env/fan1", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, SOUNDFLAG_LOOP | SOUNDFLAG_WRAP, SOUNDFX_POS(-5.0, 1.0, -20.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/env/vent1", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, SOUNDFLAG_LOOP | SOUNDFLAG_WRAP, SOUNDFX_POS(-5.0, 1.0, -20.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/env/drip1", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, SOUNDFLAG_LOOP | SOUNDFLAG_WRAP, SOUNDFX_POS(0.0, -1.0, -20.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/env/buzz1", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_map, test, SOUNDFLAG_LOOP | SOUNDFLAG_WRAP, SOUNDFX_POS(5.0, 1.0, -20.0));
-        rlsRc(test, false);
-    }
-    if ((test = getRc(RC_SOUND, "sounds/env/drip2", &audiostate.soundrcopt, 0, NULL))) {
-        playSound(testemt_obj, test, SOUNDFLAG_LOOP);
-        rlsRc(test, false);
-    }
-    //editSoundEnv(SOUNDENV_REVERB(0.01, 1.0, 0.5, 0.25, 0.1));
-    editSoundEnv(SOUNDENV_REVERB(0.07, 0.99, 0.75, 0.6, 0.15));
+    //setAudioEnv(AUDIOENVMASK_REVERB, &(struct audioenv){.reverb = {0.01, 1.0, 0.5, 0.25, 0.1}});
+    //setAudioEnv(AUDIOENVMASK_REVERB, &(struct audioenv){.reverb = {0.07, 0.99, 0.75, 0.6, 0.15}});
 
     // TODO: cleanup
     setInputMode(INPUTMODE_INGAME);
@@ -428,7 +392,7 @@ static void unstrap(void) {
     #endif
 
     plog(LL_INFO, "Deinitializing audio manager...");
-    quitAudio();
+    quitAudio(true);
     plog(LL_INFO, "Deinitializing input manager...");
     quitInput();
     plog(LL_INFO, "Deinitializing renderer...");
@@ -490,31 +454,8 @@ static void loop(void) {
     #if DEBUG(1)
     prof_begin(&dbgprof, DBGPROF_AUDIO);
     #endif
-    editAudioEmitter(testemt_obj, false, SOUNDFX_POS(sin(t * 2.5) * 3.0, 0.0, cos(t * 2.5) * 3.0));
-    #if 0
-    {
-        float v = sin(t * 0.989);
-        if (v >= 0.25f) {
-            editSoundEnv(SOUNDENV_LPFILT(v - 0.25f), SOUNDENV_HPFILT(0.0));
-        } else if (v <= -0.25f) {
-            editSoundEnv(SOUNDENV_HPFILT(v * -1.0f - 0.25f), SOUNDENV_LPFILT(0.0));
-        } else {
-            editSoundEnv(SOUNDENV_LPFILT(0.0), SOUNDENV_HPFILT(0.0));
-        }
-    }
-    #endif
-    #if 0
-    {
-        float v = sin(t * 0.989);
-        if (v >= 0.25f) {
-            editAudioEmitter(testemt_obj, false, SOUNDFX_LPFILT(v - 0.25f), SOUNDFX_HPFILT(0.0));
-        } else if (v <= -0.25f) {
-            editAudioEmitter(testemt_obj, false, SOUNDFX_HPFILT(v * -1.0f - 0.25f), SOUNDFX_LPFILT(0.0));
-        } else {
-            editAudioEmitter(testemt_obj, false, SOUNDFX_LPFILT(0.0), SOUNDFX_HPFILT(0.0));
-        }
-    }
-    #endif
+    //editAudioEmitter(testemt_obj, false, SOUNDFX_POS(sin(t * 2.5) * 3.0, 0.0, cos(t * 2.5) * 3.0));
+    (void)t;
     #if DEBUG(1)
     prof_end(&dbgprof);
     #endif
@@ -587,7 +528,7 @@ static void loop(void) {
     #if DEBUG(1)
     prof_begin(&dbgprof, DBGPROF_AUDIO);
     #endif
-    updateSounds(framemult);
+    updateAudio(framemult);
     #if DEBUG(1)
     prof_begin(&dbgprof, DBGPROF_RENDERER);
     #endif
